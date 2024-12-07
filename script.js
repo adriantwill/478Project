e1b600ac4df582622ecdd0869364dbf8ee83cb1a
Vedansh-vas <142138679+Vedansh-vas@users.noreply.github.com>
2024-12-06 18:45:58 -0700

    Update script.js
    updated size of the area graph

--- script.js
+++ script.js
@@ -518,7 +518,7 @@ function createRadialChart(data) {
 
 d3.csv("Stats_populatiy.csv").then((data) => {
   const margin = { top: 50, right: 20, bottom: 50, left: 0 };
-  const width = 1400 - margin.left - margin.right;
+  const width = 1200 - margin.left - margin.right;
   const height = 700 - margin.top - margin.bottom;
 
   const svg  = d3
